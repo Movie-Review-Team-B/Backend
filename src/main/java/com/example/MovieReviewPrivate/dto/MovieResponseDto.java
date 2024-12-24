008827c70e3a4d74718e1e73646745ffb75f67fe
seunggonhyeon <hyunsg5638@gmail.com>
2024-12-24 12:37:35 +0900

    feat:ResponseDto생성

--- src/main/java/com/example/MovieReviewPrivate/dto/MovieResponseDto.java
+++ src/main/java/com/example/MovieReviewPrivate/dto/MovieResponseDto.java
@@ -1,31 +1,28 @@
 package com.example.MovieReviewPrivate.dto;
 
-import jakarta.persistence.Column;
-
 import java.util.Date;
 
-public class MovieRequestDto {
-    // id는 Entity에서 자동으로 생성
-
+public class MovieResponseDto {
+    private final Long id;
     private final String title;
-
     private final float rating;
-
-    private final Boolean isScreening;
-
+    private final boolean isScreening;
     private final String genre;
-
     private final Date releaseDate;
-
     private final Date closeDate;
 
-    public MovieRequestDto(String title, float rating, Boolean isScreening, String genre, Date releaseDate, Date closeDate) {
-        this.title = title;
-        this.rating = rating;
-        this.isScreening = isScreening;
-        this.genre = genre;
-        this.releaseDate = releaseDate;
-        this.closeDate = closeDate;
+    public MovieResponseDto(Long id, String title, float rating, boolean isScreening, String genre, Date releaseDate, Date closeDate) {
+        this.id=id;
+        this.title=title;
+        this.rating=rating;
+        this.isScreening=isScreening;
+        this.genre=genre;
+        this.releaseDate=releaseDate;
+        this.closeDate=closeDate;
+    }
+
+    public Long getId() {
+        return id;
     }
 
     public String getTitle() {
@@ -34,21 +31,16 @@ public class MovieRequestDto {
     public float getRating() {
         return rating;
     }
-
-    public Boolean getIsScreening() {
+    public boolean isScreening() {
         return isScreening;
     }
-
     public String getGenre() {
         return genre;
     }
-
     public Date getReleaseDate() {
         return releaseDate;
     }
-
     public Date getCloseDate() {
         return closeDate;
     }
-
 }
